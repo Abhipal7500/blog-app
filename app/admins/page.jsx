053446f1a0bb85e4dcc0d1011704cfc31200146f
Abhipal7500 <131875068+Abhipal7500@users.noreply.github.com>
2025-02-15 13:55:47 +0530

    done

--- app/admins/page.jsx
+++ app/admins/page.jsx
@@ -1,18 +1,7 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+
 
 export default function AdminPage() {
-    const router = useRouter();
 
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        const tokenExpiry = localStorage.getItem("tokenExpiry");
-
-        if (!token || !tokenExpiry || Date.now() > tokenExpiry) {
-            router.push("/login");  // Redirect if no valid token
-        }
-    }, []);
 
     return <h1>Welcome to Admin Dashboard</h1>;
 }

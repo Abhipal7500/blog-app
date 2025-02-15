5fabfce7a26055d5e15227f7be81e0511332c9a6
Abhipal7500 <131875068+Abhipal7500@users.noreply.github.com>
2025-02-15 18:12:55 +0530

    g

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -1,8 +1,6 @@
 import { ConnectDB } from "@/lib/config/db";
 import EmailModel from "@/lib/models/EmailModel";
 import { NextResponse } from "next/server";
-import redis from "@/lib/redis"; 
-
 
 async function ensureDBConnection() {
   if (!global.isDBConnected) {
@@ -23,8 +21,6 @@ export async function POST(request) {
     const emailData = { email: body.email };
     await EmailModel.create(emailData);
 
-    await redis.del("emails");
-
     return NextResponse.json({ success: true, msg: "Email Subscribed" }, { status: 201 });
   } catch (error) {
     console.error("Error subscribing email:", error);
@@ -36,16 +32,7 @@ export async function GET() {
   try {
     await ensureDBConnection();
 
-    const cachedEmails = await redis.get("emails");
-    if (cachedEmails) {
-      console.log("Cache hit - Returning cached data");
-      return NextResponse.json({ emails: JSON.parse(cachedEmails) }, { status: 200 });
-    }
-
-    console.log("Cache miss - Fetching from database");
     const emails = await EmailModel.find({});
-
-    await redis.set("emails", JSON.stringify(emails), "EX", 600);
 
     return NextResponse.json({ emails }, { status: 200 });
   } catch (error) {
@@ -64,9 +51,6 @@ export async function DELETE(request) {
     }
 
     await EmailModel.findByIdAndDelete(id);
-
-
-    await redis.del("emails");
 
     return NextResponse.json({ success: true, msg: "Email Deleted" }, { status: 200 });
   } catch (error) {
